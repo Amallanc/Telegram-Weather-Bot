2814232fef33c11a681d066c3a06ca836d6afc90
Cyrus <wiranenar@gmail.com>
2018-01-29 21:57:09 +0300

    bot reports the weather

--- bot.js
+++ bot.js
@@ -15,3 +15,15 @@ const kelvin = 273.15;
 //You should choose your own openweathermap id
 const appId = config.get('appId');
 const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?appid=${appId}&q=${sity}`;
+
+const bot = new Bot(token, { polling: true });
+
+bot.onText(/\/weather/, (msg, [source, match]) => {
+    const chatId = msg.chat.id;
+
+    request.get(weatherUrl, (err, res, body) => {
+        if(err) { throw new Error(err); }
+        let data = JSON.parse(body);
+        bot.sendMessage(chatId, `${msg['chat']['first_name']}, погода в Краснодаре составляет ${(data['main']['temp'] - kelvin).toFixed(1)} °C`);
+    });
+});
